ffed864b26791d912c5f04266da60b21a4984a90
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-08 19:49:44 +0800

    Update WPSDocerVIPuserCrack.js

--- WPSDocerVIPuserCrack.js
+++ WPSDocerVIPuserCrack.js
@@ -5,15 +5,13 @@
 下载地址：http://t.cn/A6ZD2hl3
 脚本作者：Hausd0rff
 更新时间：2022-07-05
-脚本发布：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_777
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 [rewrite_local]
 # > WPS解锁稻壳会员
-^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPuserCrack.js
-^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPowerCrack.js
+^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/donglovexiao/Rewrite/main/WPSDocerVIPuserCrack.js
+^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/donglovexiao/Rewrite/main/WPSDocerVIPowerCrack.js
 
 [mitm] 
 hostname = *.docer.wps.cn, vipapi.wps.cn, account.wps.cn
