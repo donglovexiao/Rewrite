d64d5f49d9b253bfd3291dd372979f11de87116c
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-08 19:26:19 +0800

    Update 612.js

--- 612.js
+++ 612.js
@@ -2,12 +2,15 @@
 
 Author：@ddgksf2013
 
-通知频道：https://t.me/ddgksf2021
 
 
 APP：B612咔叽
+[rewrite_local]
+# > B612 解锁会员订阅
+https://user-kaji-api.b612kaji.com/v1/purchase/subscription/subscriber/status url script-response-body https://raw.githubusercontent.com/donglovexiao/Rewrite/main/612.js
 
-https://user-kaji-api.b612kaji.com/v1/purchase/subscription/subscriber/status
+[mitm] 
+hostname = user-kaji-api.b612kaji.com
 
 */
  
