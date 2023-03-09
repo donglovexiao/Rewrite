b89e4346af465529f4902f79334cd48d2aa6b0a7
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-09 08:38:47 +0800

    Update CamscannerPro.js

--- CamscannerPro.js
+++ CamscannerPro.js
@@ -3,12 +3,14 @@
 软件版本：6.18.0
 使用声明：⚠️仅供学习交流，🈲️商业用途
 *******************************
+
 [rewrite_local]
 # > 扫描全能王
 ^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/donglovexiao/Rewrite/main/CamscannerPro.js
 
 [mitm] 
 hostname = ap*.intsig.net
+
 *******************************/
 
 
