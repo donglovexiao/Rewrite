1662bf6ddeba871fa3fc19466fc4a2bd2b210d53
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-10 10:32:09 +0800

    Create wufan.js

---
+++ wufan.js
@@ -0,0 +1,15 @@
+/*******************************
+
+脚本名称:悟饭
+
+下载地址:https://apps.apple.com/cn/app/id1549154479
+
+*******************************
+[rewrite_local]
+
+^http[s]?:\/\/.+user/ios_store_pay_edition.+(check_members_identity_v2|get_member_info) url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/wufan.js
+
+[mitm] 
+hostname = iosv2.cjapi.wufan88.com
+
+*******************************/
