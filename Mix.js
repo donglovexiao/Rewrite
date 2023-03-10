2404f4122b8d8cafb7203d950a49d3e561aeeb2d
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-10 13:52:13 +0800

    Create Mix.js

---
+++ Mix.js
@@ -0,0 +1,14 @@
+/*
+by @hiepkimcdtk55
+
+* MIX解锁订阅功能
+* 配置好脚本、点击三天试用、恢复购买、取消订阅。
+=======Quantumult X=======
+
+[rewrite_local]
+^https:\/\/bmall\.camera360\.com\/api\/mix\/recovery$ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/Mix.js
+
+[MITM]
+hostname = bmall.camera360.com
+
+*/
