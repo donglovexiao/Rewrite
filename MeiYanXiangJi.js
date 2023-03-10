705919cf7fdc5f762239e05fb8c393b33c5c554e
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-10 12:15:40 +0800

    Update MeiYanXiangJi.js

--- MeiYanXiangJi.js
+++ MeiYanXiangJi.js
@@ -7,7 +7,8 @@
 
 # > 美颜相机-v10.3.00(2022.01.15)
 ^https?:\/\/api\.account\.meitu\.com\/(common\/check_device_login_pwd|users\/show_current\.json) url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/MeiYanXiangJi.js
+^https?:\/\/api\.meiyan\.com\/vip\/(user_center\.json|user_info\.json) url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/MeiYanXiangJi.js
 
 [mitm] 
-hostname = api.account.meitu.com
+hostname = api.account.meitu.com, api.meiyan.com
 *******************************/
