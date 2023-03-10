0b959374d6cb3bd1bbfcd962dc39aae164ddcab2
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-10 10:38:07 +0800

    Update WPSDocerVIPuserCrack.js

--- WPSDocerVIPuserCrack.js
+++ WPSDocerVIPuserCrack.js
@@ -18,20 +18,3 @@ hostname = *.docer.wps.cn, vipapi.wps.cn, account.wps.cn
 
 *******************************/
 
-var body = $response.body;
-var objc = JSON.parse(body);
-
-objc.vips = [
-    {
-      "memberid" : 12,
-      "expire_time" : 32495476149,
-      "name" : "稻壳会员",
-      "has_ad" : 0,
-      "enabled" : null
-    }
-];
-
-body = JSON.stringify(objc);
-$done({ 
-    body 
-});
