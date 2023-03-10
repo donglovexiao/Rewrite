b06f9eab634facfc42f14a42c18233d4e87cbecb
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-10 17:39:51 +0800

    Create TopwidgetsPro.js

---
+++ TopwidgetsPro.js
@@ -0,0 +1,14 @@
+/******************************
+脚本功能：TopWidgets解锁专业版
+软件版本：商店【最新版】
+使用声明：⚠️仅供学习交流，🈲️商业用途
+*******************************
+[rewrite_local]
+# ✅ TopWidgets
+^https?:\/\/.*xiaozujian\.com\/api\/app\/config\/userConfig url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/TopwidgetsPro.js
+
+[mitm] 
+hostname = *xiaozujian
+*******************************/
+
+
