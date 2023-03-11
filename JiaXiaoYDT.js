ad9d4b2ac909e82eb9561cc9045e528686e574e8
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-11 09:24:44 +0800

    Create JiaXiaoYDT.js

---
+++ JiaXiaoYDT.js
@@ -0,0 +1,20 @@
+/******************************
+
+脚本功能：驾校一点通+解锁VIP
+特别注意：必须v10.6.2版本
+软件版本：v10.6.2
+脚本作者：彭于晏
+更新时间：2022-9-8
+TG反馈群：https://t.me/plus8889
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/vipapi\.jxedt\.com\/store\/vip\/check url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/JiaXiaoYDT.js
+
+[mitm] 
+hostname = vipapi.jxedt.com
+
+*******************************/
