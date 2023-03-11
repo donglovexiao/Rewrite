62a955230665bec3fa8e62880b08ea53060a6358
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-11 09:16:24 +0800

    Update BaiMiao.js

--- BaiMiao.js
+++ BaiMiao.js
@@ -5,17 +5,14 @@
 软件版本：3.3.7
 脚本作者：彭于晏💞
 更新时间：2022-9-29
-问题反馈：QQ+89996462
 QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
 
 [rewrite_local]
 
-^https?:\/\/baimiao\.uzero\.cn\/api\/v\d\.user\/appLaunchWithUser$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/baimiao.js
+^https?:\/\/baimiao\.uzero\.cn\/api\/v\d\.user\/appLaunchWithUser$ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/BaiMiao.js
 
 [mitm] 
 
