99046447eb354ef686a1ca687e9537b4dc890f98
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-11 09:35:05 +0800

    Create YinZhangSCQ.js

---
+++ YinZhangSCQ.js
@@ -0,0 +1,24 @@
+/******************************
+
+脚本功能：印章生成器+解锁订阅
+下载地址：https://is.gd/neA2XY
+软件版本：2.0.0
+脚本作者：彭于晏💞
+更新时间：2022-10-1
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/YinZhangSCQ.js
+
+[mitm] 
+
+hostname = buy.itunes.apple.com
+
+*******************************/
