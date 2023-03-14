9d476445eaf552cfd2a7b1d4592301152bd8e7ed
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-14 19:47:04 +0800

    Create ColorfulWidget.js

---
+++ ColorfulWidget.js
@@ -0,0 +1,21 @@
+/******************************
+
+脚本功能：Colorful—Widget+解锁订阅
+下载地址：https://is.gd/5nkGAB
+软件版本：6.7.1
+脚本作者：彭于晏💞
+更新时间：2023-3-10
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/ColorfulWidget.js
+
+[mitm] 
+
+hostname = buy.itunes.apple.com
+
+
+*******************************/
