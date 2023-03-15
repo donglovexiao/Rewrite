e0321469d20aa11ea933c50047138a9eb22e483b
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 08:20:31 +0800

    Create Picsart.js

---
+++ Picsart.js
@@ -0,0 +1,20 @@
+/******************************
+
+脚本功能：Picsart 美易——解锁VIP
+下载地址：https://is.gd/XXUsD9
+软件版本：18.3.3
+脚本作者：彭于晏💞
+更新时间：2022-11-16
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/api\.meiease\.cn\/shop\/subscription\/validate url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/Picsart.js
+
+[mitm] 
+
+hostname = api.meiease.cn
+
+*******************************/
