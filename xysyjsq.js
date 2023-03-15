216b3b1f4337ced954526c3d07ca26389bb91947
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 10:18:26 +0800

    迅游手游加速器

---
+++ xysyjsq.js
@@ -0,0 +1,19 @@
+/******************************
+
+脚本功能：迅游手游加速器+解锁VIP
+下载地址：http://mtw.so/6cjeYf
+软件版本：4.7.4
+脚本作者：彭于晏
+更新时间：2022-9-1
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/api\.xunyou\.mobi\/api\/v1\/android\/sessions url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/xysyjsq.js
+
+[mitm] 
+hostname = api.xunyou.mobi
+
+*******************************/
