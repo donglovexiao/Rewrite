bb8f6141ece9b486426b878dbfa1dfda0866b4d9
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 09:46:41 +0800

    网易有道词典

---
+++ wyydcd.js
@@ -0,0 +1,20 @@
+/******************************
+
+脚本功能：网易有道词典+解锁VIP
+下载地址：https://is.gd/6rXQNp
+软件版本：9.2.27
+脚本作者：彭于晏
+更新时间：2022-9-23
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/dict\.youdao\.com\/vip\/user\/status url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/wyydcd.js
+
+[mitm] 
+
+hostname = dict.youdao.com
+
+*******************************/
