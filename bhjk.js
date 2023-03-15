fa1cb76d6daad8db03fcc85266ec8f87a1776349
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 08:39:28 +0800

    薄荷健康

---
+++ bhjk.js
@@ -0,0 +1,21 @@
+/******************************
+
+脚本功能：薄荷健康+解锁超级会员
+下载地址：https://is.gd/1DlXCk
+软件版本：11.2.2
+脚本作者：彭于晏💞
+更新时间：2023-3-10
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https?:\/\/api\.boohee\.com\/app-interface\/.+\/user\/user_info url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/bhjk.js
+
+[mitm] 
+
+hostname = api.boohee.com
+
+
+*******************************/
