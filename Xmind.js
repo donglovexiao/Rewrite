47251103a1abdc967ef697a5c3b0731c225867e0
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 10:15:34 +0800

    Create Xmind.js

---
+++ Xmind.js
@@ -0,0 +1,20 @@
+/******************************
+
+脚本功能：XMind思维导图+解锁订阅
+下载地址：http://mtw.so/64S2u0
+软件版本：3.2.10
+脚本作者：彭于晏
+更新时间：2022-9-2
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/www\.xmind\.cn\/_res\/devices url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/Xmind.js
+
+[mitm] 
+hostname = www.xmind.cn
+
+*******************************/
