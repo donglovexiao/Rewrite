93823193106ed6b5c8384a424d3015c62a4065fc
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 15:15:19 +0800

    B612咔叽

--- 612.js
+++ 612.js
@@ -1,9 +1,7 @@
 /*
-脚本功能：B612解锁会员订阅
+脚本功能：B612咔叽解锁会员订阅
 Author：@ddgksf2013
 
-
-APP：B612咔叽
 [rewrite_local]
 # > B612 解锁会员订阅
 https://user-kaji-api.b612kaji.com/v1/purchase/subscription/subscriber/status url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/b612.js
