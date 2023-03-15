a79b3d1c55e908993c95149ba715754833e5487e
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 08:33:31 +0800

    AI换脸秀

---
+++ aihlx.js
@@ -0,0 +1,19 @@
+/******************************
+
+脚本功能：AI换脸秀+解锁VIP
+下载地址：http://mtw.so/5PvhLM
+软件版本：1.1.1
+脚本作者：彭于晏
+更新时间：2022-9-18
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+
+*******************************
+[rewrite_local]
+
+^https:\/\/changeclothes\.szsszykj\.com\/interface\/GetIosVip.php url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/aihlx.js
+
+[mitm]
+hostname = changeclothes.szsszykj.com
+
+*******************************/
