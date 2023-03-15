7297b269b3173870b6c96ad5657870463a956774
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 08:31:25 +0800

    小熊录屏

---
+++ XXLP.js
@@ -0,0 +1,21 @@
+/******************************
+
+脚本功能：小熊录屏——解锁VIP
+特别说明：需要试用一次！！！
+下载地址：https://is.gd/V8NNal
+软件版本：1.7.6
+脚本作者：彭于晏💞
+更新时间：2023-2-28
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/donate-api.recorder.duapps.com url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/XXLP.js
+
+[mitm] 
+
+hostname = donate-api.recorder.duapps.com
+
+*******************************/
