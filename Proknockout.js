54e465387854528a94f32f4258a41b0c45fce4cf
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 08:15:48 +0800

    Create Proknockout.js

---
+++ Proknockout.js
@@ -0,0 +1,20 @@
+/******************************
+
+脚本功能：Proknockout扣图神器+解锁VIP
+下载地址：http://mtw.so/6rugdA
+软件版本：7.12
+脚本作者：彭于晏💞
+更新时间：2022-11-4
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/Proknockout.js
+
+[mitm] 
+
+hostname = buy.itunes.apple.com
+
+*******************************/
