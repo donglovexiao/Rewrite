0f2080488fdcb5ba24cfc289144ba25077fb4dff
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 14:55:42 +0800

    菜谱大全

--- CaiPuDaQuan.js
+++ CaiPuDaQuan.js
@@ -3,6 +3,7 @@
 软件版本：5.0.8
 使用声明：⚠️仅供学习交流，🈲️商业用途
 *******************************
+
 [rewrite_local]
 # > 菜谱大全-v5.0.8(2022.01.19)
 ^https?:\/\/apiios_dq\.xiangha\.com\/v\d\/ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/CaiPuDaQuan.js
@@ -10,4 +11,5 @@
 
 [mitm] 
 hostname = apiios.xiangha.com, apiios_dq.xiangha.com, appweb_dq.xiangha.com
+
 *******************************/
