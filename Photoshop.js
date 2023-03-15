277de72c8a949e80bb920df268c4d57cc8b577b2
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 08:17:06 +0800

    Create Photoshop.js

---
+++ Photoshop.js
@@ -0,0 +1,22 @@
+/******************************
+
+脚本功能：Photoshop Express +解锁高级用户
+使用说明：【软件重新下载，先开规则，在开软件】
+下载地址：http://u6v.cn/6qQw0V
+软件版本：22.35.0
+脚本作者：彭于晏
+更新时间：2022-9-9
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/lcs-mobile-cops\.adobe\.io\/mobile_profile url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/Photoshop.js
+
+[mitm] 
+
+hostname = lcs-mobile-cops.adobe.io
+
+*******************************/
