fdd793d284ae21f45e13f831c0ef597a35d5372c
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 08:05:39 +0800

    Create Nicegram.js

---
+++ Nicegram.js
@@ -0,0 +1,14 @@
+/******************************
+脚本功能：Nicegram: Unlimited Text+解锁订阅
+下载地址：https://is.gd/7OPpId
+软件版本：1.1.2
+脚本作者：彭于晏💞
+更新时间：2022-10-8
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+*******************************
+[rewrite_local]
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/donglovexiao/Script/main//Nicegram.js
+
+[mitm] 
+hostname=restore-access.indream.app
+*******************************/
