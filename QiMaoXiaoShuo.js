e9b2070abfcffe4d0156bfecc4bdba0d69e32365
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 14:58:38 +0800

    七猫小说

--- QiMaoXiaoShuo.js
+++ QiMaoXiaoShuo.js
@@ -3,10 +3,12 @@
 软件版本：5.13
 使用声明：⚠️仅供学习交流，🈲️商业用途
 *******************************
+
 [rewrite_local]
 # > 七猫小说(2022.01.18)
 ^https?:\/\/(api-\w+|xiaoshuo)\.wtzw\.com\/api\/v\d\/ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/QiMaoXiaoShuo.js
 
 [mitm] 
 hostname = *.wtzw.com
+
 *******************************/
