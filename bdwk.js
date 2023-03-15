971f8dec849d6b87e10a70a081dae3e461870011
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-15 08:41:06 +0800

    百度文库

---
+++ bdwk.js
@@ -0,0 +1,20 @@
+/******************************
+
+脚本功能：百度文库☆解锁VIP文档阅读权限
+下载地址：https://is.gd/NYKRaD
+软件版本：8.1.01
+脚本作者：彭于晏💞
+更新时间：2022-10-21
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/appwk\.baidu\.com\/naapi\/user\/getinfo url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/bdwk.js
+
+[mitm]
+
+hostname = appwk.baidu.com
+
+*******************************/
