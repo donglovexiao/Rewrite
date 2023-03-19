921d6cbafba7c3d01827db85694fd47f96522752
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-19 18:33:44 +0800

    Update AoRuan.js

--- AoRuan.js
+++ AoRuan.js
@@ -1,11 +1,14 @@
 ##############################################
 /*
-# > 脚本功能：傲软抠图解锁会员
-# > TG频道：https://t.me/ddgksf2013
-# > Only for own use, created by ddgksf2013
-# > 更新时间：2023-02-08
-# > 适用版本：适用商店【最新版】
-# > https://github.com/ddgksf2013/dev/raw/master/ForOwnUse.conf
+> 应用名称：傲软抠图
+> 软件版本：1.6.5
+> 下载地址：https://apps.apple.com/cn/app/id1490054676
+> 脚本作者：Cuttlefish
+> 微信账号：墨鱼手记
+> 更新时间：2023-03-19
+> 通知频道：https://t.me/ddgksf2021
+> 问题反馈：https://t.me/ddgksf2013_bot
+> 特别说明：本脚本仅供学习交流使用，禁止转载售卖
 
 [rewrite_local]
 # ✅ 傲软抠图(need登录)
