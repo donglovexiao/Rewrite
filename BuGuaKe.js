c8326a6a7c1eed508519f12c7a825dc70154bff8
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-03-22 17:32:33 +0800

    不挂科

---
+++ BuGuaKe.js
@@ -0,0 +1,21 @@
+/******************************
+✅ 不挂科 - 文库大学生版
+
+脚本功能：解锁永久会员🔓
+ ⁃ 解锁会员文档阅读
+ ⁃ 解锁全部搜题权限
+ ⁃ 其他更多自行探索
+ 
+▎脚本作者：@Sweet510
+▎支持版本：商店最新 v2.4.20
+使用声明：⚠️仅供学习交流，🈲️商业用途
+*******************************
+[rewrite_local]
+
+^https:\/\/appwk\.baidu\.com\/bgk\/(user\/getinfo|doc\/(getdocdownloadcopywriter|quedocsearch\?)) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/BuGuaKe.js
+
+[mitm] 
+
+hostname = appwk.baidu.com
+
+*******************************/
