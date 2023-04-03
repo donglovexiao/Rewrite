6c34db3e75d44d1ef6b55942de7cd4c6c5c16472
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-04-03 19:18:58 +0800

    B站繁体CC字幕转中文简体

---
+++ bilibili_cc.js
@@ -0,0 +1,23 @@
+/***********************************
+
+> 应用名称：哔哩哔哩
+> 软件版本：7.1.0
+> 下载地址：https://apps.apple.com/cn/app/id736536022
+> 脚本作者：Cuttlefish
+> 微信账号：墨鱼手记
+> 更新时间：2022-11-22
+> 通知频道：https://t.me/ddgksf2021
+> 投稿助手：https://t.me/ddgksf2013_bot
+> 问题反馈：📮 ddgksf2013@163.com 📮
+> 特别说明：本脚本仅供学习交流使用，禁止转载售卖
+ 
+[rewrite_local]
+
+# ～ 嗶哩嗶哩繁体CC字幕转中文简体（2022-10-12）@ddgksf2013
+^https?:\/\/i.\.hdslb\.com\/bfs\/subtitle\/.+\.json$ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/bilibili_cc.js
+
+[mitm]
+
+hostname=i0.hdslb.com
+
+***********************************/
