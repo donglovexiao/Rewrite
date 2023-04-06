09febde9c85bb2b6727adf66e84402c4a2479cc5
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-04-06 10:56:26 +0800

    爱剪辑

---
+++ aijianji.js
@@ -0,0 +1,14 @@
+/******************************
+脚本功能：爱剪辑-视屏剪辑&视屏编辑vlog制作 解锁会员(需要登录)
+软件版本：11.6.35
+脚本作者：@ddgksf2013
+更新时间：2022.11.26
+使用声明：⚠️仅供学习交流，🈲️商业用途
+*******************************
+
+[rewrite_local]
+^https?:\/\/api\.open\.loveclip\.site\/UserInfo\/(UserPersonalCoreAsync|GetUserDetail) url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/aijianji.js
+
+hostname = api.open.loveclip.site
+
+*******************************/
