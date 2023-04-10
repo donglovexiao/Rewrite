6e6717f1001eeb0ba54df94f316e50b21b2cf457
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-04-10 17:49:13 +0800

    计算器HD

---
+++ CalculatorHD.js
@@ -0,0 +1,16 @@
+/******************************
+脚本功能：计算器HD解锁会员订阅
+软件版本：1.9.7
+脚本作者：@ddgksf2013
+更新时间：2023-04-07
+使用声明：⚠️仅供学习交流，🈲️商业用途
+*******************************
+
+[rewrite_local]
+# > 计算器HD 解锁会员订阅
+^https:\/\/www\.40sishi\.com\/calculator\/user\/profile$ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/CalculatorHD.js
+
+[mitm] 
+hostname = www.40sishi.com
+
+*******************************/
