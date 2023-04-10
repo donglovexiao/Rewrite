9d0c9f3896e3239b164a5925e36d2689acb5000a
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-04-10 18:41:24 +0800

    Create alicloud.js

---
+++ alicloud.js
@@ -0,0 +1,17 @@
+/*************************************
+
+项目名称：阿里云盘 净化/解锁视频倍数+码率
+下载地址：https://t.cn/A6GqTACm
+脚本作者：chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+使用说明：非一次性解锁，每次解锁需打开脚本
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/(api|member)\.aliyundrive\.com url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/alicloud.js
+
+[mitm]
+hostname = *.aliyundrive.com
+
+*************************************/
