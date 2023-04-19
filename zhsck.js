7612abb327aabb21ba807481c1aebc02ff6c60a9
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2023-04-19 08:32:50 +0800

    Update zhsck.js

--- zhsck.js
+++ zhsck.js
@@ -11,10 +11,13 @@
 
 [rewrite_local]
 
-http:\/\/api\.chuangqi\.store\/ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/zhsck.js
+http:\/\/api\.chuangqi\.store\/ url script-response-body https://raw.githubusercontent.com/donglovexiao/Rewrite/main/zhsck.js
 
 [mitm] 
 
 hostname = api.chuangqi.store
 
 *******************************/
+
+body = $response.body.replace(/\"vip_end_time":\w+/g, '\"vip_end_time":4099040228000').replace(/\"is_ad_vip":\d+/g, '\"is_ad_vip":1').replace(/\"isvip":\d+/g, '\"isvip":1')
+$done({body});
