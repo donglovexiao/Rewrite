d00852e8e26939cab1ae2ac94480fef7c1780e94
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2024-02-17 13:57:56 +0800

    JavDB解锁永久会员.js

--- JavDB.js
+++ JavDB.js
@@ -1,31 +1,18 @@
 /******************************
-
-脚本功能：JavDB解锁会员
-软件版本：1.9.12
-官网地址：https://javdb.com/
-下载地址：https://github.com/bdvajstudio/javdb/releases
+脚本功能：javdb 解锁永久会员
+软件版本：1.9.16
+下载地址：https://jav.app
 脚本作者：Hausd0rff
-更新时间：2022-09-24
-电报频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_777
-使用声明：⚠️此脚本仅供学习与交流，
-        请勿转载与贩卖！⚠️⚠️⚠️
-脚本说明：注册时填写邀请码：rckasw
-        可无限延长脚本的使用期限
+更新时间：2023-05-26
+脚本发布：https://t.me/yqc_123
+使用声明：‼️脚本仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-# > JavDB解锁会员
-^https?:\/\/.+\/api\/v1\/(movies\/.*\/play|startup) url script-request-header https://raw.githubusercontent.com/donglovexiao/Rewrite/main/JavDB.js
-
+# > javdb 解锁永久会员
+^https?:\/\/.*\/api\/v1\/users url script-response-body https://raw.githubusercontent.com/donglovexiao/Rewrite/main/JavDB.js
+^https?:\/\/.*\/api\/v1\/(movies\/.*\/play|startup) url script-request-header https://raw.githubusercontent.com/donglovexiao/Rewrite/main/JavDB.js
 [mitm] 
-hostname = shukego.com, yiliang8888.com, 91mingyang.com, mnter.com, f5bc.club
-
+hostname = api.pxxgg.xyz, api.ujvnmkx.cn, jdforrepam.com, api.yijingluowangluo.xyz, api.wwwuh5.cn, api.hechuangxinxi.xyz
 *******************************/
 
-var _0xhrff6 = ["\x68\x65\x61\x64\x65\x72\x73", "\x61\x75\x74\x68\x6F\x72\x69\x7A\x61\x74\x69\x6F\x6E", "\x42\x65\x61\x72\x65\x72\x20\x65\x79\x4A\x68\x62\x47\x63\x69\x4F\x69\x4A\x49\x55\x7A\x49\x31\x4E\x69\x4A\x39\x2E\x65\x79\x4A\x70\x5A\x43\x49\x36\x4D\x54\x51\x30\x4D\x6A\x51\x34\x4D\x79\x77\x69\x64\x58\x4E\x6C\x63\x6D\x35\x68\x62\x57\x55\x69\x4F\x69\x4A\x35\x63\x57\x4D\x77\x4D\x44\x63\x69\x66\x51\x2E\x59\x44\x75\x6A\x50\x35\x53\x44\x37\x36\x6F\x6D\x35\x75\x51\x71\x74\x56\x75\x47\x2D\x64\x33\x6A\x44\x7A\x68\x4F\x31\x4A\x69\x35\x53\x48\x56\x64\x70\x74\x36\x4E\x57\x4A\x34", "\x73\x69\x67\x6E\x61\x74\x75\x72\x65", "\x34\x35\x61\x65\x62\x66\x32\x32\x33\x39\x66\x35\x33\x33\x36\x31\x31\x62\x31\x37\x32\x61\x38\x65\x63\x66\x66\x30\x33\x36\x61\x33"];
-var hausd0rff = $request[_0xhrff6[0x0]];
-hausd0rff[_0xhrff6[0x1]] = _0xhrff6[0x2];
-hausd0rff[_0xhrff6[0x3]] = _0xhrff6[0x4];
-$done({
-    headers : hausd0rff
-});
+var __encode ='https://t.me/yqc_123',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Oxfdd96=["\x62\x6F\x64\x79","\x70\x61\x72\x73\x65","\x69\x73\x5F\x76\x69\x70","\x75\x73\x65\x72","\x64\x61\x74\x61","\x75\x73\x65\x72\x6E\x61\x6D\x65","\uD835\uDC3B\uD835\uDC4E\uD835\uDC62\uD835\uDC60\uD835\uDC51\uD835\uDC5C\uD835\uDC5F\uD835\uDC53\uD835\uDC53","\x76\x69\x70\x5F\x65\x78\x70\x69\x72\x65\x64\x5F\x61\x74","\x32\x31\x30\x31\x2D\x30\x36\x2D\x30\x38\x54\x31\x37\x3A\x33\x35\x3A\x30\x31\x2E\x30\x30\x30\x2B\x30\x38\x3A\x30\x30","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];var body=$response[__Oxfdd96[0x0]];var hausd0rff=JSON[__Oxfdd96[0x1]](body);hausd0rff[__Oxfdd96[0x4]][__Oxfdd96[0x3]][__Oxfdd96[0x2]]= true;hausd0rff[__Oxfdd96[0x4]][__Oxfdd96[0x3]][__Oxfdd96[0x5]]= __Oxfdd96[0x6];hausd0rff[__Oxfdd96[0x4]][__Oxfdd96[0x3]][__Oxfdd96[0x7]]= __Oxfdd96[0x8];$done({body:JSON[__Oxfdd96[0x9]](hausd0rff)});;;(function(_0x8656x3,_0x8656x4,_0x8656x5,_0x8656x6,_0x8656x7,_0x8656x8){_0x8656x8= __Oxfdd96[0xa];_0x8656x6= function(_0x8656x9){if( typeof alert!== _0x8656x8){alert(_0x8656x9)};if( typeof console!== _0x8656x8){console[__Oxfdd96[0xb]](_0x8656x9)}};_0x8656x5= function(_0x8656xa,_0x8656x3){return _0x8656xa+ _0x8656x3};_0x8656x7= _0x8656x5(__Oxfdd96[0xc],_0x8656x5(_0x8656x5(__Oxfdd96[0xd],__Oxfdd96[0xe]),__Oxfdd96[0xf]));try{_0x8656x3= __encode;if(!( typeof _0x8656x3!== _0x8656x8&& _0x8656x3=== _0x8656x5(__Oxfdd96[0x10],__Oxfdd96[0x11]))){_0x8656x6(_0x8656x7)}}catch(e){_0x8656x6(_0x8656x7)}})({})
