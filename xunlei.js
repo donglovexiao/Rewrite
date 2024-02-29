ed17f481b115807ae2fcb4e488aaac28cbcd0184
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2024-02-29 19:48:38 +0800

    迅雷 解锁SVIP

--- xunlei.js
+++ xunlei.js
@@ -8,33 +8,11 @@
 
 [rewrite_local]
 # > 迅雷 解锁 SVIP
-https://xluser-ssl.xunlei.com/xluser.core.login/v3/getuserinfo url script-response-body https://raw.githubusercontent.com/donglovexiao/Rewrite/main/xunlei.js
+https://xluser-ssl.xunlei.com/xluser.core.login/v3/getuserinfo url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/xunlei.js
 
 [mitm] 
 hostname = xluser-ssl.xunlei.com
 
 */
 ##############################################
-
-var body = $response.body;
-var obj = JSON.parse(body);
-
-obj.vipList = [{
-    "expireDate": "20290609",
-    "isAutoDeduct": "0",
-    "isVip": "1",
-    "isYear": "1",
-    "payId": "0",
-    "payName": "---",
-    "register": "0",
-    "vasid": "2",
-    "vasType": "5",
-    "vipDayGrow": "20",
-    "vipGrow": "840",
-    "vipLevel": "7"
-  }]
-
-body = JSON.stringify(obj);
-$done({body});
-
 
