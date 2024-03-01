6f7204841dd1d8420577ad264a921c3dd7ec2689
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2024-03-01 16:44:15 +0800

    Update TouchRetouch.js

--- TouchRetouch.js
+++ TouchRetouch.js
@@ -4,16 +4,17 @@
 
 脚本功能：TouchRetouch - 解锁订阅
 下载地址：https://is.gd/8rSPp1
-软件版本：5.1.2
-脚本作者：彭于晏💞
-更新时间：2023-3-3
+软件版本：5.1.23
+脚本作者：Hausd0rff
+更新时间：2023-09-02
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/TouchRetouch.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/\$RCAnonymousID%3A\w{32}|receipts)$ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/TouchRetouch.js
+
 
 [mitm] 
 
