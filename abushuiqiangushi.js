513efa4512bfbc6f101cb8193319291508d8587a
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2024-03-05 10:47:55 +0800

    阿布睡前故事

--- abushuiqiangushi.js
+++ abushuiqiangushi.js
@@ -4,14 +4,14 @@
 下载地址：https://is.gd/6Q6lRw
 软件版本：1.2.9
 脚本作者：彭于晏💞
-更新时间：2022-10-01
+更新时间：2023-8-23
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
 
 [rewrite_local]
 
-http:\/\/tutugushiapi\.tutuerge\.com url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/abushuiqiangushi.js
+^http[s]?:\/\/tutugushiapi.tutuerge.com\/default.aspx url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/abushuiqiangushi.js
 
 [mitm] 
 
