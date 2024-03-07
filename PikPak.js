d500afd1cec071815fbb70ea6a97ae4343a8c58d
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2024-03-07 08:52:15 +0800

    Update PikPak.js

--- PikPak.js
+++ PikPak.js
@@ -1,8 +1,9 @@
 /******************************
 
 脚本功能：PikPak 解锁会员订阅
-软件版本：1.11.0
-更新时间：2023-04-16
+软件下载：https://apps.apple.com/us/app/pikpak-private-cloud-saver/id1616861537?l=en-GB
+软件版本：1.22.3
+更新时间：2024-02-16
 使用声明：⚠️仅供学习交流，🈲️商业用途
 
 *******************************
@@ -12,33 +13,34 @@
 ^https:\/\/api-drive\.mypikpak\.com\/(vip\/v\d\/(vip\/info|allSubscriptionStatus)|drive\/v\d\/about\?space) url script-response-body https://raw.githubusercontent.com/donglovexiao/Rewrite/main/PikPak.js
 
 [mitm] 
-hostname = api-drive.mypikpak.com
+hostname = *.mypikpak.com
 
 *******************************/
 
 var url = $request.url;
-var modified = JSON.parse($response.body);
+var objc = JSON.parse($response.body);
 const URL1 = '/vip/info';
 const URL2 = '/about';
 const URL3 = '/allSubscriptionStatus';
 if (url.indexOf(URL1) != -1) {
-  modified.data.expire = "2099-01-01T00:00:00+08:00";
-  modified.data.status = "ok";
-  modified.data.type = "platinum";
-  modified.data.vipItem = [
+  objc.data.expire = "2099-09-09T00:00:00+09:00";
+  objc.data.status = "ok";
+  objc.data.type = "platinum";
+  objc.data.vipItem = [
       {
         "status" : "ok",
-        "expire" : "2099-01-01T00:00:00+08:00",
+        "expire" : "2099-09-09T00:00:00+09:00",
         "type" : "regional",
-        "description" : "区域会员",
-        "surplus_day" : 510004015
+        "description" : "Regional members",
+        "surplus_day" : 747364014
       }
     ];
 };
 if (url.indexOf(URL2) != -1) {
-  modified.quota.limit = "10995116277760";
+  objc.quota.limit = "10999166278790";
+	objc.quota.is_unlimited = true;
 };
 if (url.indexOf(URL3) != -1) {
-  modified.apple = {"subscribed":true,"purchased":true,"status":"trial","interval":"year","product":"sub.year","past_due_deadline":"","pay_type":"","region":"regional"};
+  objc.apple = {"subscribed":true,"purchased":true,"status":"trial","interval":"year","product":"sub.year","past_due_deadline":"","pay_type":"","region":"regional"};
 };
-$done({body:JSON.stringify(modified)});
+$done({body:JSON.stringify(objc)});
