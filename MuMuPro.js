38bd6964fe0f7c82dd0e832615fb48e0ba6ac120
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2024-03-21 19:02:42 +0800

    MuMu模拟器Pro Mac版

---
+++ MuMuPro.js
@@ -0,0 +1,19 @@
+/******************************
+
+脚本功能：MuMu Pro解锁会员
+软件版本：v1.3.12
+下载地址：https://mumu.163.com/mac/
+脚本作者：Dong
+更新时间：2024-3-21
+使用声明：⚠️仅供学习交流，🈲️商业用途
+
+*******************************
+
+[rewrite_local]
+# > MuMu Pro解锁会员
+^https:\/\/api-pro\.mumu\.163\.com\/api\/v1\/user\/info url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/mumu.js
+
+[mitm] 
+hostname = *.mumu.163.com
+
+*******************************/
