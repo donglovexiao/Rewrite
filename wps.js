59d78bfb3a6b5b7cf0b369243365fdc5c86c8128
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2025-02-18 19:35:15 +0800

    WPS Office 解锁会员

---
+++ wps.js
@@ -0,0 +1,22 @@
+
+/******************************
+
+脚本功能：WPS Office 解锁会员功能
+软件版本：商店最新版-11.35.0
+下载地址：http://t.cn/A6KnWtUu
+脚本作者：510004015
+更新时间：2024-2-23
+使用声明：⚠️仅供学习交流，🈲️商业用途
+
+***************************
+QuantumultX:
+
+[rewrite_local]
+
+^https:\/\/(vip|vas)\.wps\.cn\/((v2\/vip_center\/my\/privilege)|(query\/api\/v1\/(list_purchase_info|list_privilege_info))) url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/wps.js
+
+[mitm] 
+
+hostname = vas.wps.cn, vip.wps.cn
+
+***************************/
