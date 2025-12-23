64229f78ed6db680a485d70e761d03edc4b1f44f
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2025-12-23 18:12:47 +0800

    Update Picsew.js

--- Picsew.js
+++ Picsew.js
@@ -2,8 +2,8 @@
 
 脚本功能：Picsew解锁专业版
 软件版本：3.9.4
-下载地址：http://t.cn/Aig753CC
-脚本作者：Passer_by_yun
+下载地址：https://apps.apple.com/app/id1208145167
+脚本作者：Guding88
 更新时间：2022-10-14
 使用声明：⚠️仅供学习交流，🈲️商业用途
 
@@ -11,7 +11,7 @@
 
 [rewrite_local]
 # > Picsew解锁专业版
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/PicsewPro.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/donglovexiao/Rewrite/main/Picsew.js
 
 
 [mitm] 
@@ -19,3 +19,26 @@ hostname = buy.itunes.apple.com
 
 *******************************/
 
+var guding = {
+  "receipt": {
+    "receipt_type": "Production",
+    "bundle_id": "com.sugarmo.ScrollClip",
+    "in_app": [
+      {
+        "quantity": "1",
+        "purchase_date_ms": "1669275526000",
+        "transaction_id": "300001282466542",
+        "original_transaction_id": "300001282466542",
+        "product_id": "com.sugarmo.ScrollClip.pro",
+        "in_app_ownership_type": "PURCHASED",
+        "original_purchase_date_ms": "1669275526000"
+      }
+    ],
+    "application_version": "3206",
+    "original_purchase_date_ms": "1669275302000",
+    "original_application_version": "3206",
+  },
+  "environment": "Production",
+  "status": 0,
+}
+$done({ body: JSON.stringify(guding) });
