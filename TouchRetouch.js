5ad3b06171082f5cae96c4f1ef69698d067f9e06
donglovexiao <127277977+donglovexiao@users.noreply.github.com>
2025-12-23 18:25:47 +0800

    Update TouchRetouch.js

--- TouchRetouch.js
+++ TouchRetouch.js
@@ -1,19 +1,19 @@
 
-
 /******************************
 
-脚本功能：TouchRetouch - 解锁订阅
-下载地址：https://is.gd/8rSPp1
-软件版本：5.1.23
-脚本作者：Hausd0rff
-更新时间：2024-02-26
+脚本功能：TouchRetouch - 解锁会员订阅
+下载地址：https://apps.apple.com/app/id373311252
+软件版本：5.5.4
+脚本作者：Guding88
+更新时间：2025-12-12
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/\$RCAnonymousID%3A\w{32}|receipts)$ url script-response-body https://raw.githubusercontent.com/donglovexiao/Script/main/TouchRetouch.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/donglovexiao/Rewrite/main/TouchRetouch.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/donglovexiao/Rewrite/main/TouchRetouch.js
 
 
 [mitm] 
@@ -22,3 +22,29 @@ hostname = api.revenuecat.com
 
 *******************************/
 
+const guding = {};
+const guding6 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  guding.headers = $request.headers;
+} else if (guding6 && guding6.subscriber) {
+  guding6.subscriber.subscriptions = guding6.subscriber.subscriptions || {};
+  guding6.subscriber.entitlement = guding6.subscriber.entitlement || {};
+  const app = 'gd';const list = {'gd':{name: 'premium', id: 'tr5_yearlysubsc_20dlrs_1'}};
+  const data = {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"};
+for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
+guding6.subscriber.subscriptions[list[i].id] = data;
+guding6.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+guding6.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+                break;
+          }
+    }
+    guding.body = JSON.stringify(guding6);
+}
+$done(guding);
